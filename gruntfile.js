f2cc4e5ee3451407f8341d6dd20fd2a2a117c71d
bud_ha <hmh.uhtala11@gmail.com>
2015-06-08 16:43:07 +0300

    grunt node-inspector task remove

--- gruntfile.js
+++ gruntfile.js
@@ -67,19 +67,6 @@ module.exports = function(grunt) {
         }
       }
     },
-    'node-inspector': {
-      custom: {
-        options: {
-          'web-port': 1337,
-          'web-host': 'localhost',
-          'debug-port': 5858,
-          'save-live-edit': true,
-          'no-preload': true,
-          'stack-trace-limit': 50,
-          'hidden': []
-        }
-      }
-    },
     concurrent: {
       default: ['nodemon', 'watch'],
       debug: ['nodemon', 'watch', 'node-inspector'],
